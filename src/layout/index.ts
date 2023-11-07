d4c68e895531386cf6e0a995d924f081ad5fb9cf
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-07 14:38:14 +0300

    export layout components

---
+++ src/layout/index.ts
@@ -0,0 +1,4 @@
+export { Bottombar } from './Bottombar/Bottombar'
+export { LeftSidebar } from './LeftSidebar/LeftSidebar'
+export { RightSidebar } from './RightSidebar/RightSidebar'
+export { Topbar } from './Topbar/Topbar'
