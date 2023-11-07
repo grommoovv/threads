f27299227a46b08d113d9e7934b709c5e8b0756b
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-07 14:17:25 +0300

    sign-up page

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/nextjs'
+
+const Page = () => {
+  return (
+    <div className='flex h-screen w-full items-center justify-center'>
+      <SignUp />
+    </div>
+  )
+}
+
+export default Page
