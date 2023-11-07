c1258f198268f5170d45cc1f5368fb9060a671c5
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-07 14:06:25 +0300

    root group layout

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,36 @@
+import React, { FC, PropsWithChildren } from 'react'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { Topbar, LeftSidebar, RightSidebar, Bottombar } from '@/layout'
+import '../globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Threads',
+  description: 'A Threads Application developed with Next.js & TypeScript',
+}
+
+const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
+
+export default RootLayout
