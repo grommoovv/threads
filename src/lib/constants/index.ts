0c36926f23c8578d622284adacdb24cc9359c666
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-30 23:31:47 +0300

    edit label

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,7 +2,7 @@ const sidebarLinks = [
   { imgURL: '/assets/home.svg', route: '/', label: 'Home' },
   { imgURL: '/assets/search.svg', route: '/search', label: 'Search' },
   { imgURL: '/assets/heart.svg', route: '/activity', label: 'Activity' },
-  { imgURL: '/assets/create.svg', route: '/create-thread', label: 'Create Thread' },
+  { imgURL: '/assets/create.svg', route: '/create-thread', label: 'Create' },
   { imgURL: '/assets/community.svg', route: '/communities', label: 'Communities' },
   { imgURL: '/assets/user.svg', route: '/profile', label: 'Profile' },
 ]
